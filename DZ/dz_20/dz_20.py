9b94f763765044a29e9f6221d8d7996d84bf4083
Vsitkov <vsitkov99@gmail.com>
2022-11-22 23:43:20 +0500

    22.11.2022

--- DZ/dz_20/dz_20.py
+++ DZ/dz_20/dz_20.py
@@ -13,21 +13,21 @@ import re
 #
 # # 2я Задача, вроде получилось -----------------------------------------------
 #
-# n = "MMDCCLXXIII"
-# n1 = "CCCMMVIIVV"
-#
-#
-# def num(a):
-#     # return re.findall(r'(M|C|X|I|)*', a)
-#     # reg = re.findall(r'^M{0,3}(C{0,3}D?|CM|DC)?(X{0,3}L?|XC|LX)?(I{0,3}V?|IX|IV)?$', a)
-#     reg = re.findall(r'^(M{0,3})(CM|CD|D?C{0,3})?(XC|XL|L?X{0,3})?(IX|IV|V?I{0,3})?$', a)
-#     # print(reg)
-#     if reg:
-#         print(a, '- это римское число')
-#     else:
-#         print(a, '- это не римское число')
-#
-#
-# num(n)
-# num(n1)
+n = "MMDCCLXXIII"
+n1 = "CCCMMVIIVV"
+
+
+def num(a):
+    # return re.findall(r'(M|C|X|I|)*', a)
+    # reg = re.findall(r'^M{0,3}(C{0,3}D?|CM|DC)?(X{0,3}L?|XC|LX)?(I{0,3}V?|IX|IV)?$', a)
+    reg = re.findall(r'^M{0,3}(CM|CD|D?C{0,3})?(XC|XL|L?X{0,3})?(IX|IV|V?I{0,3})?$', a)
+    # print(reg)
+    if reg:
+        print(a, '- это римское число')
+    else:
+        print(a, '- это не римское число')
+
+
+num(n)
+num(n1)
 
--- main.py
+++ main.py
@@ -3836,4 +3836,51 @@ import re
 
 # 22.11.2022 ===================================================================================
 
-print('hello')
+# print('hello')
+
+# s = "Я ищу совпадения в 2023 году. И я их найду в 2 счёта."
+# reg = r'([0-9]+)\s(\D+)'
+# print(re.search(reg, s).group())
+# m = re.search(reg, s)
+# print('[1]', m[1])
+# print('[2]', m[2])
+# print('[0]', m[0])
+# print(re.search(reg, s).group(1))
+
+# text = '''
+# Самара
+# Москва
+# Тверь
+# Уфа
+# Казань
+# '''
+#
+# count = 0
+#
+#
+# def replace_find(m):
+#     global count
+#     count += 1
+#     return f'<option value="{count}">{m.group(1)}</option>\n'
+#
+#
+# print(re.sub(r'\s*(\w+)\s*', replace_find, text))
+
+# s = "<p>Изображение <img src='bg.jpg'> - фон страницы</p>"
+# reg = r'<img\s+[^>]*src=([\'"])(.+)\1>'
+# print(re.findall(reg, s))
+
+# (?P<name>...)  (?P=name)  назначение имени для круглой скобки
+
+# s = "<p>Изображение <img src='bg.jpg'> - фон страницы</p>"
+# reg = r'<img\s+[^>]*src=(?P<q>[\'"])(.+)(?P=q)>'
+# print(re.findall(reg, s))
+
+# s = "Самолет прилетает 10/23/2023. Будем ради вас видеть после 10/24/2023."
+# reg = r"(\d{2})/(\d{2})/(\d{4})"
+# print(re.findall(reg, s))
+# print(re.sub(reg, r'\2.\1.\3', s))
+
+s = 'google.com and google.ru and yandex.ru'
+reg = r'(([a-z0-9-]{2,}\.)+[a-z]{2,4})'
+print(re.sub(reg, r'http://\1', s))
