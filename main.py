05631e219a2d814e415e72316dbae9d666e0c190
Vsitkov <vsitkov99@gmail.com>
2022-12-14 00:00:59 +0500

    14.12.22

--- main.py
+++ main.py
@@ -3672,7 +3672,7 @@
 # * - от 0 до бесконечности
 # ? - 0 или 1
 
-import re
+# import re
 
 # s = "Я ищу совпадения в 2023 году. И я их найду в 2 счёта. 9875 19_4 5"
 # # reg = r'\A\w\s\w+\s\w+'  # w - Я
@@ -4184,7 +4184,7 @@ import re
 #         fw.write(line)
 
 # Модуль OS и OS.PATH ------------------------------------------------
-import os
+# import os
 
 
 # print('Текущая директория: ', os.getcwd())  # D:\PythonAcademy
@@ -4587,3 +4587,245 @@ import os
 # # p1.__y = 'abc'
 # # print(p1.__x, p1.__y)
 # print(p1.__dict__)
+
+# 13.12.2022 ============================================================================================
+
+# class Point:
+#     __slots__ = ['__x', '__y', 'z']
+#
+#     def __init__(self, x, y):
+#         self.__x = self.__y = 0
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x  # _Point__x
+#             self.__y = y
+#
+#     def __check_value(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def set_coord(self, x, y):
+#         if Point.__check_value(x) and Point.__check_value(y):
+#             self.__x = x
+#             self.__y = y
+#         else:
+#             print('Координаты должны быть числами')
+#
+#     def get_cord(self):
+#         return self.__x, self.__y
+#
+#
+# p1 = Point('5', 10)
+# p1.z = 15
+# print(p1.z)
+# # print(p1.__dict__)
+
+# class Point:
+#     def __init__(self, x=0, y=0):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __set_x(self, x):  # устанавливаем
+#         print('Вызов __set.x')
+#         self.__x = x
+#
+#     def __get_x(self):  # получаем
+#         print('Вызов __get_x')
+#         return self.__x
+#
+#     def __del_x(self):
+#         print('Удаление свойства')
+#         del self.__x
+#
+#     x = property(__get_x, __set_x, __del_x)
+#
+#
+# p1 = Point(5, 10)
+# p1.x = 100
+# print(p1.x)
+# del p1.x
+# print()
+
+# class Point:
+#     def __init__(self, x=0, y=0):
+#         self.__x = x
+#         self.__y = y
+#
+#     @property
+#     def x(self):  # получаем  __get_x
+#         print('Вызов __get_x')
+#         return self.__x
+#
+#     @x.setter
+#     def x(self, x):  # устанавливаем   __set_x
+#         print('Вызов __set.x')
+#         self.__x = x
+#
+#     @x.deleter
+#     def x(self):  # __del_x
+#         print('Удаление свойства')
+#         del self.__x
+#
+#     # x = property(__get_x, __set_x, __del_x)
+#
+#
+# p1 = Point(5, 10)
+# p1.x = 100
+# # print(p1.__dict__)
+# print(p1.x)
+# # del p1.x
+
+# class KgToPounds:
+#     def __init__(self, kg):
+#         self.__kg = kg
+#
+#     @property
+#     def kg(self):
+#         return self.__kg
+#
+#     @kg.setter
+#     def kg(self, new_kg):
+#         if isinstance(new_kg, (int, float)):
+#             self.__kg = new_kg
+#         else:
+#             print('Килограммы задаются только числами')
+#
+#     def to_pounds(self):
+#         return self.__kg * 2.205
+#
+#
+# weight = KgToPounds(12)
+# print(weight.kg, 'кг =>', end=' ')
+# print(weight.to_pounds(), 'фунтов')
+# weight.kg = 41
+# print(weight.kg, 'кг =>', end=' ')
+# print(weight.to_pounds(), 'фунтов')
+# weight.kg = '41'
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.__name = name
+#         self.__age = age
+#
+#     @property
+#     def name(self):
+#         return self.__name
+#
+#     @name.setter
+#     def name(self, new_name):
+#         self.__name = new_name
+#
+#     @name.deleter
+#     def name(self):
+#         del self.__name
+#
+#     @property
+#     def age(self):
+#         return self.__age
+#
+#     @age.setter
+#     def age(self, new_age):
+#         self.__age = new_age
+#
+#     @age.deleter
+#     def age(self):
+#         del self.__age
+#
+#
+# p1 = Person('Irina', 26)
+# print(p1.name, p1.age)
+# p1.name = 'Igor'
+# p1.age = 31
+# print(p1.name, p1.age)
+# del p1.age
+
+# class Point:
+#     __count = 0
+#
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+#         Point.__count += 1
+#
+#     @staticmethod
+#     def get_count():
+#         return Point.__count
+#
+#     # get_count = staticmethod(get_count)  # старый метод
+#
+#
+# p1 = Point(5, 10)
+# p2 = Point(4, 8)
+# p3 = Point(2, 7)
+# print(Point.get_count())
+# print(p1.get_count())
+
+# class Change:
+#     @staticmethod
+#     def inc(x):
+#         return x + 1
+#
+#     @staticmethod
+#     def dec(x):
+#         return x - 1
+#
+#
+# print(Change.inc(10), Change.dec(10))
+
+# class Args:
+#     @staticmethod
+#     def min(*args):
+#         return min(args)
+#
+#     @staticmethod
+#     def max(*args):
+#         return max(args)
+#
+#     @staticmethod
+#     def arf(*args):
+#         return sum(args) / len(args)
+#
+#     @staticmethod
+#     def f(x):
+#         one = 1
+#         for i in range(1, x + 1):
+#             one *= i
+#         return one
+#
+#
+# print(Args.min(3, 5, 7, 9))
+# print(Args.max(3, 5, 7, 9))
+# print(Args.arf(3, 5, 7, 9))
+# print(Args.f(5))
+
+# class Numbers:
+#     @staticmethod
+#     def max(*args):
+#         max1 = 0
+#         for i in args:
+#             max1 = max1 if i < max1 else i
+#         return max1
+#
+#
+# print(Numbers.max(3, 5, 7, 9, 4, 16, 1))
+
+# class Date:
+#     def __init__(self, day, month, year):
+#         self.day = day
+#         self.month = month
+#         self.year = year
+#
+#     @classmethod
+#     def from_string(cls, date_as_string):
+#         day, month, year = map(int, date_as_string.split('.'))
+#         date1 = cls(day, month, year)
+#         return date1
+#
+#     def string_to_db(self):
+#         return f'{self.year}-{self.month}-{self.day}'
+#
+#
+# string_date = Date.from_string('23.10.2022')
+# print(string_date.string_to_db())
+# string_date1 = Date.from_string('23.12.2021')
+# print(string_date1.string_to_db())
