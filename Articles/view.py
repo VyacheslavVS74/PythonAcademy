3e9a61825b2579c8a0ff5e9d9019daf4fc5a5691
Vsitkov <vsitkov99@gmail.com>
2023-02-13 14:58:36 +0500

    dz_44 13.02.2023

--- Articles/view.py
+++ Articles/view.py
@@ -1,11 +1,19 @@
-def add_title():
-    ...
+
+def add_title(args1):
+    def args_dec(fn):
+        def wrap(*args, **kwargs):
+            print(args1.center(50, "="))
+            a = fn(*args, **kwargs)
+            print("=" * 50)
+            return a
+        return wrap
+    return args_dec
+
 
 class UserInterface:
 
-    # @add_title("Ввод пользовательских данных")
+    @add_title("Ввод пользовательских данных")
     def wait_user_answer(self):
-        print(" Ввод пользовательских данных ".center(50, "="))
         print("Действие со статьями:")
         print("1 - Создание статьи"
               "\n2 - Просмотр статей"
@@ -13,10 +21,9 @@ class UserInterface:
               "\n4 - удаление статьи"
               "\nq - выход из программы")
         user_answer = input("Выберите вариант действия: ")
-        print("=" * 50)
         return user_answer
 
-    # @add_title("Создание статьи:")
+    @add_title("Создание статьи:")
     def add_user_article(self):
         dict_article = {
             "название статьи": None,
@@ -24,15 +31,12 @@ class UserInterface:
             "количество страниц": None,
             "описание": None
         }
-        print(" Создание статьи: ".center(50, "="))
         for key in dict_article:
             dict_article[key] = input(f"Введите {key} статьи: ")
-        print("=" * 50)
+
         return dict_article
 
-    # @add_title("Список статей:")
+    @add_title("Список статей:")
     def show_all_article(self, articles):
-        print("Список статей: ".center(50, "="))
         for ind, article in enumerate(articles, start=1):
             print(f"{ind}. {article}")
-        print("=" * 50)
--- main.py
+++ main.py
@@ -7445,87 +7445,87 @@
 # if __name__ == "__main__":
 #     main()
 
-import requests
-from bs4 import BeautifulSoup
-import re
-import csv
-
-
-def get_html(url):
-    r = requests.get(url)
-    return r.text
-
-
-def refine_cy(s):
-    return s.split()[-1]
-
-
-def refine_snippet(s):
-    res = [ord(c) for c in s if ord(c) < 9000]
-    res1 = ''.join([chr(c) for c in res])
-    return res1
-
-
-def write_csv(data):
-    with open("plugins1.csv", "a", encoding="utf-8-sig") as f:
-        writer = csv.writer(f, lineterminator="\r", delimiter=";")
-        writer.writerow((data["name"],
-                         data["url"],
-                         data["snippet"],
-                         data["cy"]))
-
-
-def get_gata(html):
-    soup = BeautifulSoup(html, "lxml")
-    elements = soup.find_all("article", class_="plugin-card")
-    # print(len(elements))
-    for el in elements:
-        try:
-            name = el.find("h3").text
-        except ValueError:
-            name = ""
-
-        try:
-            url = el.find("h3").find("a").get("href")
-        except ValueError:
-            url = ""
-
-        try:
-            snippet = el.find("div", class_="entry-excerpt").text.strip()
-            snippet1 = refine_snippet(snippet)
-        except ValueError:
-            snippet1 = ""
-
-        try:
-            c = el.find("span", class_="tested-with").text.strip()
-            cy = refine_cy(c)
-        except ValueError:
-            cy = ""
-
-        data = {
-            "name": name,
-            "url": url,
-            "snippet": snippet1,
-            "cy": cy
-        }
-
-        write_csv(data)
-
-        # print(data)
-        # print(cy)
-        # print(snippet1)
-        # print(url)
-        # print(name)
-
-
-def main():
-    for i in range(0, 25):
-        url = f"https://ru.wordpress.org/plugins/browse/blocks/page/{i}/"
-        get_gata(get_html(url))
-
-
-if __name__ == "__main__":
-    main()
+# import requests
+# from bs4 import BeautifulSoup
+# import re
+# import csv
+#
+#
+# def get_html(url):
+#     r = requests.get(url)
+#     return r.text
+#
+#
+# def refine_cy(s):
+#     return s.split()[-1]
+#
+#
+# def refine_snippet(s):
+#     res = [ord(c) for c in s if ord(c) < 9000]
+#     res1 = ''.join([chr(c) for c in res])
+#     return res1
+#
+#
+# def write_csv(data):
+#     with open("plugins1.csv", "a", encoding="utf-8-sig") as f:
+#         writer = csv.writer(f, lineterminator="\r", delimiter=";")
+#         writer.writerow((data["name"],
+#                          data["url"],
+#                          data["snippet"],
+#                          data["cy"]))
+#
+#
+# def get_gata(html):
+#     soup = BeautifulSoup(html, "lxml")
+#     elements = soup.find_all("article", class_="plugin-card")
+#     # print(len(elements))
+#     for el in elements:
+#         try:
+#             name = el.find("h3").text
+#         except ValueError:
+#             name = ""
+#
+#         try:
+#             url = el.find("h3").find("a").get("href")
+#         except ValueError:
+#             url = ""
+#
+#         try:
+#             snippet = el.find("div", class_="entry-excerpt").text.strip()
+#             snippet1 = refine_snippet(snippet)
+#         except ValueError:
+#             snippet1 = ""
+#
+#         try:
+#             c = el.find("span", class_="tested-with").text.strip()
+#             cy = refine_cy(c)
+#         except ValueError:
+#             cy = ""
+#
+#         data = {
+#             "name": name,
+#             "url": url,
+#             "snippet": snippet1,
+#             "cy": cy
+#         }
+#
+#         write_csv(data)
+#
+#         # print(data)
+#         # print(cy)
+#         # print(snippet1)
+#         # print(url)
+#         # print(name)
+#
+#
+# def main():
+#     for i in range(0, 25):
+#         url = f"https://ru.wordpress.org/plugins/browse/blocks/page/{i}/"
+#         get_gata(get_html(url))
+#
+#
+# if __name__ == "__main__":
+#     main()
 
 # 09.02.2023 ================================================================================
 
