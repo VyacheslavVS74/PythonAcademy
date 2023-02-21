34d346d70ba8a728bbc92b4b43973adaad1c8068
Vsitkov <vsitkov99@gmail.com>
2023-02-21 23:32:52 +0500

    21.02.2023

--- BD/Bd.py
+++ BD/Bd.py
@@ -56,17 +56,18 @@
 
 # ORDER BY имя поля [ASC|DESC] - сортировка
 
-import sqlite3 as sq
-
-with sq.connect("users.db") as con:
-    cur = con.cursor()
-    # cur.execute("""CREATE TABLE IF NOT EXISTS person(
-    # id INTEGER PRIMARY KEY AUTOINCREMENT,
-    # name TEXT NOT NULL,
-    # phone BLOB NOT NULL DEFAULT "+79090000000",
-    # age INTEGER NOT NULL CHECK(age>15 AND age<70),
-    # email TEXT UNIQUE
-    # )""")
+# import sqlite3 as sq
+#
+# with sq.connect("users.db") as con:
+#     cur = con.cursor()
+#     cur.execute("""
+#     CREATE TABLE IF NOT EXISTS person(
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     phone BLOB NOT NULL DEFAULT "+79090000000",
+#     age INTEGER NOT NULL CHECK(age>15 AND age<70),
+#     email TEXT UNIQUE
+#     )""")
 
 # переименование таблицы ниже
 #     cur.execute("""
@@ -100,3 +101,59 @@ with sq.connect("users.db") as con:
 # Добавление новой строки ----------------
 # INSERT INTO имя_таблицы [(столбец1[, столбец_n])]
 # VALUES (значение_1[, значение_n])
+
+# 21.02.2023 ======================================================================
+
+# Перенос строки из одной таблицы в другую
+# INSERT INTO T1(ID, Fname, Doljnost, Orab, ZP)
+# SELECT ID, Fname, D, Orabot, Zp
+# From T2
+# WHERE ID LIKE 19
+
+# UPDATE имя_таблицы
+# SET столбец1=выражэение1, столбец2=выражение2 -- SET установить
+# [WHERE условие]
+
+# удаление определенных данных
+# DELETE FROM имя_таблицы
+# WHERE условие
+
+# очистить записи из таблицы
+# DELETE FROM T2;
+
+# LIMIT колличество_строк OFFSET смещение
+# limit [смещение,] количество_строк
+# SELECT *
+# FROM T1
+# ORDER BY Fname
+# LIMIT 10, 5
+# /*LIMIT 5 OFFSET 2*/
+# 5 записей со сдвигом на 2 от начала
+
+
+import sqlite3 as sq
+
+with sq.connect("db_3.db") as con:
+    cur = con.cursor()
+    cur.execute("""
+    SELECT *
+    FROM T1
+    ORDER BY Fname
+    LIMIT 2, 5
+    """)
+
+    # res = cur.fetchone()
+    res2 = cur.fetchmany(3)
+    res1 = cur.fetchall()
+    # print(res)
+    print(res2)
+    print(res1)
+
+    # res = cur.fetchall()
+    # print(res)
+
+    # for res in cur:
+    #     print(res)
+
+# разработчики логической базы данных
+# разработчики физической базы данных
