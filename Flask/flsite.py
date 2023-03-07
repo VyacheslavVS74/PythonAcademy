758f49ee6829a1f504053c8fdc43842639f9d7a0
Vsitkov <vsitkov99@gmail.com>
2023-03-07 23:51:23 +0500

    07.03.2023

---
+++ Flask/flsite.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+menu = [{"name": "Главная", "url": "index"},
+        {"name": "О нас", "url": "about"},
+        {"name": "Обратная связь", "url": "contact"}
+        ]
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    print(url_for('index'))
+    return render_template("index.html", title="Главная", menu=menu)
+
+
+@app.route("/about")
+def about():
+    print(url_for('about'))
+    return render_template('about.html', title="О нас", menu=menu)
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == "POST":
+
+        context = {
+            "username": request.form["username"],
+            "email": request.form["email"],
+            "message": request.form["message"]
+        }
+        return render_template("contact.html", **context, title="Обратная связь", menu=menu)
+        # print(request.form["username"])
+        # print(request.form)
+    return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+@app.route("/profile/<int:username>")  # int, float, path - можно использовать /
+def profile(username):
+    return f"Пользователь: {username}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- shablonizator/main.py
+++ shablonizator/main.py
@@ -185,6 +185,23 @@
 # msg = tm.render(users=persons)
 # print(msg)
 
+# from jinja2 import Environment, FileSystemLoader
+#
+# persons = [{"name": "Алексей", "year": 18, "weight": 78.5},
+#            {"name": "Никита", "year": 28, "weight": 82.3},
+#            {"name": "Виталий", "year": 33, "weight": 94.0}
+# ]
+#
+# file_loader = FileSystemLoader("templates")
+# env = Environment(loader=file_loader)
+#
+# tm = env.get_template('main.html')
+# msg = tm.render(users=persons, title='About Jinja')
+#
+# print(msg)
+
+# {% include <путь> %}
+
 from jinja2 import Environment, FileSystemLoader
 
 persons = [{"name": "Алексей", "year": 18, "weight": 78.5},
@@ -192,12 +209,12 @@ persons = [{"name": "Алексей", "year": 18, "weight": 78.5},
            {"name": "Виталий", "year": 33, "weight": 94.0}
 ]
 
+subs = ['Культура', 'Наука', 'Политика', 'Спорт']
+
 file_loader = FileSystemLoader("templates")
 env = Environment(loader=file_loader)
 
-tm = env.get_template('main.html')
-msg = tm.render(users=persons, title='About Jinja')
+tm = env.get_template('about.html')
+msg = tm.render(list_table=subs)
 
 print(msg)
-
-# {% include <путь> %}
