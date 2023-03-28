1ed1bf8d7a1848693b70aacd0f46eb829783d2b0
Vsitkov <vsitkov99@gmail.com>
2023-03-28 16:34:23 +0500

    dz_54 28.03.2023

--- DZ/dz_53/portfolio/personal_portfolio/personal_portfolio/urls.py
+++ DZ/dz_53/portfolio/personal_portfolio/personal_portfolio/urls.py
@@ -23,6 +23,10 @@ from skills import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('signup/', views.signupuser, name='signupuser'),
+    path('login/', views.loginuser, name='loginuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    # path('<int:work_id>/', views.work, name='work'),
     path('blog/', include('blog.urls')),
 ]
 
--- DZ/dz_53/portfolio/personal_portfolio/skills/views.py
+++ DZ/dz_53/portfolio/personal_portfolio/skills/views.py
@@ -1,10 +1,57 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Skills
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
 
 
 def index(request):
     projects = Skills.objects.all()
     return render(request, 'skills/index.html', {'projects': projects})
+
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'skills/loginuser.html', {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'skills/loginuser.html', {
+                'form': AuthenticationForm(),
+                'error': 'Неверные данные для входа'
+            })
+        else:
+            login(request, user)
+            return redirect('index')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('index')
+
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'skills/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('index')
+            except IntegrityError:
+                return render(request, 'skills/signupuser.html', {
+                    'form': UserCreationForm(),
+                    'error': 'Такое имя пользователя уже существует. Задайте другое'
+                })
+        else:
+            return render(request, 'skills/signupuser.html', {
+                'form': UserCreationForm(),
+                'error': 'Пароли не совпадают.'
+            })
 
 
 # def work_all(request):
