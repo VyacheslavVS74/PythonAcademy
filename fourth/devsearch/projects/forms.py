1c876c558931e0ced79241375c4144b2492e32f1
Vsitkov <vsitkov99@gmail.com>
2023-04-13 23:37:42 +0500

    13.04.2023

--- fourth/devsearch/projects/forms.py
+++ fourth/devsearch/projects/forms.py
@@ -13,8 +13,8 @@ class ProjectForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # self.fields['title'].widget.attrs.update({'class': 'input'})
-        # self.fields['description'].widget.attrs.update({'class': 'input'})
-
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
+        # self.fields['description'].widget.attrs.update({'class': 'input'})
