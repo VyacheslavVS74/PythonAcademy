bc365548696abf1b13e9805e33ae67739e146ecc
Vsitkov <vsitkov99@gmail.com>
2023-04-18 23:39:14 +0500

    18.04.2023

--- fourth/devsearch/users/forms.py
+++ fourth/devsearch/users/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+from .models import Profile, Skill
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -7,6 +9,32 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
         labels = {'first_name': 'Name'}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'email', 'username', 'shot_intro', 'bio', 'profile_image',
+                  'social_github', 'social_youtube', 'social_website']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+
+
+class SkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+        exclude = ['owner']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
