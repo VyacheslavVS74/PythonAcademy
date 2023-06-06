dcd7e4e6f30beadab65269cf1df1716e0a49b444
Vsitkov <vsitkov99@gmail.com>
2023-06-06 23:47:56 +0500

    06.06.2023

--- sixth/landing_page/landing_page/settings.py
+++ sixth/landing_page/landing_page/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'cms.apps.CmsConfig',
     'price.apps.PriceConfig',
     'crm.apps.CrmConfig',
+    'tele_bot.apps.TeleBotConfig',
 ]
 
 MIDDLEWARE = [
---
+++ sixth/landing_page/tele_bot/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TeleBotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tele_bot'
